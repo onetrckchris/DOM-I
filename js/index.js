20bfb8ff83f5443872393480f95cc5e50a771768
Chris Pelayo <chris.e.pelayo@gmail.com>
2019-05-13 14:31:37 -0700

    completed last step of assignment

--- js/index.js
+++ js/index.js
@@ -6,6 +6,8 @@ const siteContent = {
     "nav-item-4": "Features",
     "nav-item-5": "About",
     "nav-item-6": "Contact",
+    "nav-item-7": "Append",
+    "nav-item-8": "Prepend",
     "img-src": "img/logo.png"
   },
   "cta": {
@@ -47,18 +49,33 @@ let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"]);
 
 // This is going to select the whole nav.
-let nav = document.querySelectorAll('nav a');
+let nav = document.querySelector('nav');
+let navContent = document.querySelectorAll('nav a');
 let navCount = 1;
 
 // This will iterate through the nav which is a NodeList and add styling and content to each a tag in the nav.
-nav.forEach(element => {
+navContent.forEach(element => {
   element.textContent = siteContent['nav'][`nav-item-${navCount}`];
+  element.style.color = 'green';
   navCount++;
 });
 
+// STRETCH: Adding 2 new itmes to the nav.
+let appendItem = document.createElement('a');
+appendItem.setAttribute('href', '#');
+appendItem.style.color = 'green';
+appendItem.textContent = 'Append';
+nav.appendChild(appendItem);
+
+let prependItem = document.createElement('a');
+prependItem.setAttribute('href', '#');
+prependItem.style.color = 'green';
+prependItem.textContent = 'Prepend'
+nav.prepend(prependItem);
+
 // These add content to the existing elements inside of header.
 let awesomeH1 = document.querySelector('.cta h1');
-awesomeH1.textContent = siteContent['cta']['h1'];
+awesomeH1.innerHTML = siteContent['cta']['h1'].replace(/\s/g, "<br>");
 
 let awesomeButton = document.querySelector('.cta button');
 awesomeButton.textContent = siteContent['cta']['button'];
@@ -66,36 +83,28 @@ awesomeButton.textContent = siteContent['cta']['button'];
 let awesomeImg = document.querySelector('.cta img');
 awesomeImg.setAttribute('src', siteContent['cta']['img-src'])
 
-// This is adding content to main-content... So ugly.
-let featuresH4 = document.querySelectorAll('.text-content')[0].querySelector('h4');
-featuresH4.textContent = siteContent['main-content']['features-h4'];
-let featuresContent = document.querySelectorAll('.text-content')[0].querySelector('p');
-featuresContent.textContent = siteContent['main-content']['features-content'];
+// This is adding content to main-content.
+let textContentH4 = document.querySelectorAll('.text-content h4');
 
-let aboutH4 = document.querySelectorAll('.text-content')[1].querySelector('h4');
-aboutH4.textContent = siteContent['main-content']['about-h4'];
-let aboutContent = document.querySelectorAll('.text-content')[1].querySelector('p');
-aboutContent.textContent = siteContent['main-content']['about-content'];
+textContentH4[0].textContent = siteContent['main-content']['features-h4'];
+textContentH4[1].textContent = siteContent['main-content']['about-h4'];
+textContentH4[2].textContent = siteContent['main-content']['services-h4'];
+textContentH4[3].textContent = siteContent['main-content']['product-h4'];
+textContentH4[4].textContent = siteContent['main-content']['vision-h4'];
 
-let servicesH4 = document.querySelectorAll('.text-content')[2].querySelector('h4');
-servicesH4.textContent = siteContent['main-content']['services-h4'];
-let servicesContent = document.querySelectorAll('.text-content')[2].querySelector('p');
-servicesContent.textContent = siteContent['main-content']['services-content'];
+let textContentP = document.querySelectorAll('.text-content p');
 
-let productH4 = document.querySelectorAll('.text-content')[3].querySelector('h4');
-productH4.textContent = siteContent['main-content']['product-h4'];
-let productContent = document.querySelectorAll('.text-content')[3].querySelector('p');
-productContent.textContent = siteContent['main-content']['product-content'];
-
-let visionH4 = document.querySelectorAll('.text-content')[4].querySelector('h4');
-visionH4.textContent = siteContent['main-content']['vision-h4'];
-let visionContent = document.querySelectorAll('.text-content')[4].querySelector('p');
-visionContent.textContent = siteContent['main-content']['vision-content'];
+textContentP[0].textContent = siteContent['main-content']['features-content'];
+textContentP[1].textContent = siteContent['main-content']['about-content'];
+textContentP[2].textContent = siteContent['main-content']['services-content'];
+textContentP[3].textContent = siteContent['main-content']['product-content'];
+textContentP[4].textContent = siteContent['main-content']['vision-content'];
 
 // Inputting the src for the middle image.
 let mainImage = document.querySelector('.middle-img');
 mainImage.setAttribute('src', siteContent['main-content']['middle-img-src']);
 
+// Adding content to the footer.
 let contactH4 = document.querySelector('.contact h4');
 contactH4.textContent = siteContent['contact']['contact-h4'];
 
@@ -103,3 +112,7 @@ let contactContent = document.querySelectorAll('.contact p');
 contactContent[0].textContent = siteContent['contact']['address'];
 contactContent[1].textContent = siteContent['contact']['phone'];
 contactContent[2].textContent = siteContent['contact']['email'];
+
+// Adding content to copyright.
+let footerContent = document.querySelector('footer p');
+footerContent.textContent = siteContent['footer']['copyright'];
